453c76e39bafc3077dcc65f7b8829fd3254724f1
limingyuan123 <1484156313@qq.com>
2020-10-18 15:49:13 +0800

    init

---
+++ src/main/java/nnu/ogms/data_conversion_back/DataConversionBackApplication.java
@@ -0,0 +1,13 @@
+package nnu.ogms.data_conversion_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConversionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConversionBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nnu/ogms/data_conversion_back/DataConversionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package nnu.ogms.data_conversion_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConversionBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
